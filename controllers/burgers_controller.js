a5edd1b2d9a33de88fbb54f95ce387b579ebe26b
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2020-01-04 20:10:43 -0800

    set server to start listening and start creating router in controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,13 @@
 var express = require(express);
 var burger = require("../models/burger.js");
 
+var router = express.Router();
+
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
+        var hbsObject = {
+            burgers: data
+        }
+        res.render("index", hbsObject);
+    })
+})
--- server.js
+++ server.js
@@ -9,3 +9,7 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + "/public"));
 
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT)
+});
+

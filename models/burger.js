0efbb21dd2f3d22825334da1db24ed5bb5ffc73e
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2020-01-04 19:24:16 -0800

    add code that will call the ORM functions

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,27 @@
 var orm = require("../config/orm.js");
 
+var burger = {
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function (results) {
+            cb(results);
+        });
+    },
+    insertOne: function (cb) {
+        orm.insertOne("burgers", cols, vals, function (results) {
+            cb(results);
+        });
+    },
+    updateOne: function (cb) {
+        orm.updateOne("burgers", objColVals, condition, function (results) {
+            cb(results);
+        });
+    }
+
+}
+
+module.exports = burger;
+
+
+
+
+

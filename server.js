9d8642b0394b3d1173e1c1cbf9cfae7836ed10f4
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2020-01-04 16:30:31 -0800

    initialize express, set port, make app use bodyparser and serve up static assets from public

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+var app = express();
+var PORT = process.env.PORT || 3002;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + "/public"));
+

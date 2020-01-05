9e46ae2c71110de18d3a94805950845d7140f848
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2020-01-04 16:51:00 -0800

    create database with entries

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('All-American Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Double-Double Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Manchego cheese Burger');

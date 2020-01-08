ad1cb1a8489266778c60dba315ff72a52c5303bb
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2020-01-07 17:45:48 -0800

    create index and main handlebars files, and initialize handlebars in server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
 
 var app = express();
 var PORT = process.env.PORT || 3002;
@@ -8,6 +9,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname + "/public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 var burgerRoutes = require("./controllers/burgers_controller.js");
 app.use(burgerRoutes);
